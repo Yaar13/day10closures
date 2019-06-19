9c0364d08d4e7fb790e1bee164b2bb849723f8ef
parag <paraggarg66@yahoo.in>
2019-06-19 18:37:32 -0400

    Initial Commit

---
+++ day10/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  day10
+//
+//  Created by Gurparkash Kaur on 2019-06-19.
+//  Copyright © 2019 Parag Garg. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
