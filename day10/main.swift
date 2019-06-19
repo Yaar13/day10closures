ab17ec9b59ada62b246d7bd728fcb639e8632995
parag <paraggarg66@yahoo.in>
2019-06-19 19:15:46 -0400

    day10closures

--- day10/main.swift
+++ day10/main.swift
@@ -8,5 +8,48 @@
 
 import Foundation
 
-print("Hello, World!")
+func add (a: Int, b: Int) -> Int
 
+{
+
+    return a + b
+
+}
+
+func subt(a: Int, b: Int) -> Int
+
+{
+
+    return a - b
+
+}
+
+let c = add(a: 100, b: 200)
+
+print("c \(c)")
+
+
+var funcAdd :(Int, Int)-> Int
+
+funcAdd = add(a:b:)
+
+let cc = funcAdd(100,200)
+
+
+print("cc \(cc)")
+
+let funcSub = subt(a:b:)
+
+
+var s = funcSub(200,100)
+
+print("s \(s)")
+
+var mul = {
+
+    (a: Int, b: Int) -> Int in
+
+    return a * b
+}
+
+ 
